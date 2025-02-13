fae7f2b54acb4c468d2f4fafd43b8bcef7168c54
Phillip Weinberg <weinbe58@gmail.com>
2025-02-13 15:05:55 -0500

    adding docs to register

--- src/bloqade/pyqrack/reg.py
+++ src/bloqade/pyqrack/reg.py
@@ -29,14 +29,21 @@ class QubitRef:
 
 
 class CRegister(list[bool]):
+    """Runtime representation of a classical register."""
+
     def __init__(self, size: int):
         super().__init__(False for _ in range(size))
 
 
 @dataclass(frozen=True)
 class CBitRef:
+    """Object representing a reference to a classical bit."""
+
     ref: CRegister
+    """The classical register that is holding this bit."""
+
     pos: int
+    """The position of this bit in the classical register."""
 
     def set_value(self, value: bool):
         self.ref[self.pos] = value
@@ -49,12 +56,28 @@ SimRegType = TypeVar("SimRegType")
 
 
 @dataclass(frozen=True)
-class SimQRegister(QRegister, Generic[SimRegType]):
+class SimQRegister(Generic[SimRegType]):
+    """Simulation runtime value of a quantum register."""
+
+    size: int
+    """The number of qubits in this register."""
+
     sim_reg: SimRegType
+    """The register of the simulator."""
+
     addrs: tuple[int, ...]
+    """The global addresses of the qubits in this register."""
+
     qubit_state: List[QubitState]
+    """The state of each qubit in this register."""
 
     def drop(self, pos: int):
+        """Drop the qubit at the given position in-place.
+
+        Args
+            pos (int): The position of the qubit to drop.
+
+        """
         assert self.qubit_state[pos] is QubitState.Active, "Qubit already lost"
         self.qubit_state[pos] = QubitState.Lost
 
@@ -64,19 +87,33 @@ class SimQRegister(QRegister, Generic[SimRegType]):
 
 @dataclass(frozen=True)
 class SimQubitRef(QubitRef, Generic[SimRegType]):
+    """The runtime representation of a qubit reference."""
+
     ref: SimQRegister[SimRegType]
+    """The quantum register that is holding this qubit."""
+
     pos: int
+    """The position of this qubit in the quantum register."""
 
     @property
     def sim_reg(self) -> SimRegType:
+        """The register of the simulator."""
         return self.ref.sim_reg
 
     @property
     def addr(self) -> int:
+        """The global address of the qubit."""
         return self.ref.addrs[self.pos]
 
     def is_active(self) -> bool:
+        """Check if the qubit is active.
+
+        Returns
+            True if the qubit is active, False otherwise.
+
+        """
         return self.ref.qubit_state[self.pos] is QubitState.Active
 
     def drop(self):
+        """Drop the qubit in-place."""
         self.ref.drop(self.pos)
